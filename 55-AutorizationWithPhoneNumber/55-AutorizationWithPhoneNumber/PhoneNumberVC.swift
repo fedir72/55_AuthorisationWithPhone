b229878142c8ceca5622afad10f4656f9f002c67
fedir72 <fedir72@ua.fm>
2020-07-02 16:44:17 +0300

    + searchList to flags and codes

--- 55-AutorizationWithPhoneNumber/55-AutorizationWithPhoneNumber/PhoneNumberVC.swift
+++ 55-AutorizationWithPhoneNumber/55-AutorizationWithPhoneNumber/PhoneNumberVC.swift
@@ -11,9 +11,18 @@ import FirebaseAuth
 import FlagPhoneNumber
 
 class PhoneNumberVC: UIViewController {
-    @IBOutlet weak var phoneNumberTextField: FPNTextField!
     
+    //переменная для передачи номера телефона
+    var phoneNumber: String?
+    
+    //контроллер представления флажков
+    var listController: FPNCountryListViewController?
+    
+    
+    @IBOutlet weak var phoneNumberTextField: FPNTextField!
     @IBOutlet weak var fetchCodeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,8 +38,47 @@ class PhoneNumberVC: UIViewController {
         fetchCodeButton.alpha = 0.5
         fetchCodeButton.isEnabled = false
         
+        //представление флажков стран как лист
+        phoneNumberTextField.displayMode = .list
+        phoneNumberTextField.delegate = self
+        listController = FPNCountryListViewController(style: .grouped)
+        listController?.setup(repository: phoneNumberTextField.countryRepository)
+        //послевыбора страны
+        listController?.didSelect = {country in
+            self.phoneNumberTextField.setFlag(countryCode: country.code)
+        }
     }
     
     
 
+}
+//протоколы заполнения текстового поля с номером
+extension PhoneNumberVC: FPNTextFieldDelegate {
+    
+    func fpnDidSelectCountry(name: String, dialCode: String, code: String) {
+        
+    }
+    
+    func fpnDidValidatePhoneNumber(textField: FPNTextField, isValid: Bool) {
+        //если номер валиден
+        if isValid {
+            fetchCodeButton.alpha = 1
+            fetchCodeButton.isEnabled = true
+            //форматируем номер в международный
+            phoneNumber = textField.getFormattedPhoneNumber(format: .International)
+        } else {
+        //если номер не действителен
+            fetchCodeButton.alpha = 0.5
+            fetchCodeButton.isEnabled = false
+        }
+    }
+    
+    func fpnDisplayCountryList() {
+        let navigatContr = UINavigationController(rootViewController: listController!)
+        listController?.title = "страны"
+        phoneNumberTextField.text = ""
+        self.present(navigatContr, animated: true, completion: nil)
+    }
+    
+    
 }
