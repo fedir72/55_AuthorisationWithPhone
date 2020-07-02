3e6d86546d3c59614a8101cfc9a0b856ddd2a6f4
fedir72 <fedir72@ua.fm>
2020-07-02 15:24:47 +0300

    emtyproject

---
+++ 55-AutorizationWithPhoneNumber/55-AutorizationWithPhoneNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  55-AutorizationWithPhoneNumber
+//
+//  Created by fedir on 02.07.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
